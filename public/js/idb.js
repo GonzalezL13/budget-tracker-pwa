7129a430c3488c90e52b5095d896f3a61c2bf6e3
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-10-05 21:49:48 -0700

    created idb.js for offline functionality

---
+++ public/js/idb.js
@@ -0,0 +1,56 @@
+let db;
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+    const store = transaction.objectStore("new_budget");
+
+    store.add(record);
+};
+
+function uploadBudget() {
+    const transaction = db.transaction(["new_budget"], "readwrite");
+    const store = transaction.objectStore("new_budget");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => {
+                    return response.json();
+                })
+                .then(() => {
+                    const transaction = db.transaction(["new_budget"], "readwrite");
+                    const store = transaction.objectStore("new_budget");
+                    store.clear();
+                });
+        }
+    };
+}
+
+window.addEventListener('online', uploadBudget);
